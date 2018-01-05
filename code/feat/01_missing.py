6890e2e911ebc2dc2d35297849f424ee90239bff
Krishna Kalyan <krishnakalyan3@gmail.com>
2018-01-06 00:10:11 +0100

    pipeline

---
+++ code/feat/01_missing.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append("../")
+import pandas as pd
+from param_config import config
+from utils import write_data
+
+
+def get_missing(data):
+    missing_cols = data.columns[data.isnull().any()]
+    return missing_cols
+
+
+def create_missing(missing_cols, df):
+    for i in missing_cols:
+        header_str = 'missing_{}'.format(i)
+        df[header_str] = df[i].isnull().astype(int)
+    return df
+
+
+def total_missing(train, test):
+    header_str = 'total_missing'
+    train[header_str] = train.isnull().sum(axis=1)
+    test[header_str] = test.isnull().sum(axis=1)
+    return train, test
+
+
+def missing(x_train, x_test):
+    train_missing = get_missing(x_train)
+    test_missing = get_missing(x_test)
+    assert len(train_missing) == len(test_missing)
+
+    train = create_missing(train_missing.values, x_train)
+    test = create_missing(train_missing.values, x_test)
+
+    train, test = total_missing(train, test)
+    return train, test
+
+
+if __name__ == '__main__':
+    train_x = pd.read_csv(config.train_file)
+    test_x = pd.read_csv(config.test_file)
+    train_y = pd.read_csv(config.target_file)
+
+    train_x, test_x = missing(train_x, test_x)
+
+    write_data(config.a_xtrain, train_x)
+    write_data(config.a_xtest, test_x)
+    write_data(config.a_ytest, train_y)
---
+++ code/feat/02_le_imputation.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from sklearn.preprocessing import Imputer
+import sys
+sys.path.append("../")
+import pandas as pd
+from param_config import config
+from sklearn.preprocessing import LabelEncoder
+from utils import read_data
+
+# TODO
+# Imputation
+# label encoding
+# handle date
+
+
+def imputer(x_train, x_test):
+    # Unused
+    # https://groups.google.com/forum/#!msg/jpmml/sooCGcGMpnc/x-EkjOr2BAAJ
+
+    imp = Imputer(missing_values='NaN', strategy=config.imputation_stratergy, axis=0)
+    imp.fit(x_train)
+    train = imp.transform(x_train)
+    test = imp.transform(x_test)
+    return train, test
+
+
+def label_encoding(x_train, x_test, impute_data):
+
+    lbl = LabelEncoder()
+    for c in x_train.columns:
+        if x_train[c].dtype == 'object':
+
+            if c in ['date_recorded']:
+                pass
+
+            print('###### Encoding {} #####'.format(c))
+            if c in impute_data:
+                x_train[c], x_test[c] = x_train[c].fillna(impute_data[c]), x_test[c].fillna(impute_data[c])
+            else:
+                raw_data = pd.concat([x_train[c], x_test[c]])
+                lbl.fit(raw_data.values)
+                x_train[c] = lbl.transform(list(x_train[c].values))
+                x_test[c] = lbl.transform(list(x_test[c].values))
+
+    return x_train, x_test
+
+
+def most_frequent(x_train):
+    na_cols = {}
+    for c in x_train.columns:
+        if x_train[c].isnull().sum() > 0:
+            na_cols[c] = test_x[c].value_counts().index[0]
+    return na_cols
+
+
+if __name__ == '__main__':
+    train_x = read_data(config.a_xtrain)
+    test_x = read_data(config.a_xtest)
+
+    impute_data = most_frequent(train_x)
+    train_x, test_x = label_encoding(train_x, test_x, impute_data)
+    exit()
+    print(test_x['scheme_name'])
